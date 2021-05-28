d0e202b20431edf9da10600d39b2eeb344168324
soso24soso <61196412+soso24soso@users.noreply.github.com>
2021-05-28 11:07:51 +0200

    Added a docker image wrapping an express.js that returns random arrays of spells

---
+++ docker-images/express-image/src/index.js
@@ -0,0 +1,45 @@
+var Chance = require("chance");
+var chance = new Chance();
+
+var express = require("express");
+var app = express();
+
+app.get('/spell', function (req, res) {
+    res.send(generateSpells());
+});
+
+app.get('/test', function (req, res) {
+    res.send("test :)");
+});
+
+app.get('/', function (req, res) {
+    res.send("Hello " + chance.name());
+});
+
+app.listen(3000, function () {
+    console.log("Acception HTTP request on port 3000.");
+});
+
+function generateSpells() {
+    const numberOfSpells = chance.integer({
+        min: 1,
+        max: 10
+    }).valueOf();
+    console.log(numberOfSpells);
+    let spells = []
+    for (let i = 0; i < numberOfSpells; ++i) {
+        var mana = chance.integer({
+            min: 1,
+            max: 100
+        })
+        var formula = chance.word()
+        var effect = chance.sentence()
+        spells.push({
+            mana: mana,
+            formula: formula,
+            effect: effect,
+        })
+    }
+    console.log(spells)
+    return spells
+}
