7d95699f1e6b0cc573042bf58a5507e3634d55aa
soso24soso <61196412+soso24soso@users.noreply.github.com>
2021-05-29 14:55:36 +0200

    added JS script to send AJAX requests to fetch spells and update DOM

---
+++ docker-images/apache-php-image/content/js/students.js
@@ -0,0 +1,17 @@
+$(function () {
+    console.log("loading students");
+
+    function loadStudents() {
+        $.getJSON( "/api/spells/spell/", function( spells ){
+            console.log(spells);
+            var message = "No spell in the book";
+            if (spells.length > 0){
+                message = spells[0].formula + " " + spells[0].mana;
+            }
+            $(".masthead-heading").text(message);
+        }) ;
+    }
+
+    loadStudents();
+    setInterval( loadStudents, 2000);
+});
