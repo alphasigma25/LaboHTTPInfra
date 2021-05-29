132c0eab5add1c0b0d99134b6fef0f2dcdfb935c
soso24soso <61196412+soso24soso@users.noreply.github.com>
2021-05-29 20:42:37 +0200

    modifications pour le load balancing

--- docker-images/apache-reverse-proxy/templates/config-template.php
+++ docker-images/apache-reverse-proxy/templates/config-template.php
@@ -1,14 +1,26 @@
 <?php
-    $static_app = getenv('STATIC_APP');
-    $dynamic_app = getenv('DYNAMIC_APP');
+    $static_app1 = getenv('STATIC_APP1');
+    $dynamic_app1 = getenv('DYNAMIC_APP1');
+
+    $static_app2 = getenv('STATIC_APP2');
+    $dynamic_app2 = getenv('DYNAMIC_APP2');
 ?>
 <VirtualHost *:80>
 	ServerName demo.res.ch
 
-	ProxyPass '/api/spells/' 'http://<?php print "$dynamic_app"?>/'
-	ProxyPassReverse '/api/spells/' 'http://<?php print "$dynamic_app"?>/'
+    <Proxy balancer://dynamic_cluster>
+        BalancerMember 'http://<?php echo "$dynamic_app1"?>'
+        BalancerMember 'http://<?php echo "$dynamic_app2"?>'
+    </Proxy>
+    <Proxy balancer://static_cluster>
+        BalancerMember 'http://<?php echo "$static_app1"?>'
+        BalancerMember 'http://<?php echo "$static_app2"?>'
+    </Proxy>
 
-	ProxyPass '/' 'http://<?php print "$static_app"?>/'
-	ProxyPassReverse '/' 'http://<?php print "$static_app"?>/'
+	ProxyPass '/api/spells/' 'balancer://dynamic_cluster/'
+	ProxyPassReverse '/api/spells/' 'balancer://dynamic_cluster/'
+
+	ProxyPass '/' 'balancer://static_cluster/'
+	ProxyPassReverse '/' 'balancer://static_cluster/'
 
 </VirtualHost>
